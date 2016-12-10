2a34fd11bf1de9ffa2a1e69be4a29568d9884151
Sameer Adhikari <sameer.adhikari@oracle.com>
2016-12-10 11:39:41 -0800

    First commit

---
+++ src/main/java/name/adhikari/sameer/playground/LambdasTutorial.java
@@ -0,0 +1,84 @@
+package name.adhikari.sameer.playground;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Collection;
+import java.util.function.Predicate;
+
+/**
+ * Created by samadhik on 11/29/16.
+ */
+public class LambdasTutorial {
+
+    public static class LengthComparator implements Comparator<String> {
+        public int compare(String first, String second) {
+            return Integer.compare(first.length(), second.length());
+        }
+    }
+
+    public static void useLambdaForComparator() {
+        String[] strings = {"am", "crazy", "for", "some", "healthy", "nutrition"};
+        System.out.println("original array: " + Arrays.toString(strings));
+
+        Arrays.sort(strings, new LengthComparator());
+        System.out.println("sort custom comparator class: " + Arrays.toString(strings));
+
+        Arrays.sort(strings);
+        System.out.println("sort using default java sort: " + Arrays.toString(strings));
+
+        Arrays.sort(strings,
+                (String first, String second) -> {
+                    if (first.length() < second.length()) return -1;
+                    else if (first.length() > second.length()) return 1;
+                    else return 0;
+                }
+        );
+        System.out.println("sort on length using lambda1: " + Arrays.toString(strings));
+
+        Arrays.sort(strings);
+        System.out.println("sort using default java sort: " + Arrays.toString(strings));
+
+        Comparator<String> comp = (first, second) -> {
+            return Integer.compare(first.length(), second.length());
+        };
+
+        Arrays.sort(strings, comp);
+        System.out.println("sort on length using lambda2: " + Arrays.toString(strings));
+
+        Arrays.sort(strings);
+        System.out.println("sort using default java sort: " + Arrays.toString(strings));
+
+        Arrays.sort(strings,
+                (first, second) -> {
+                    return Integer.compare(first.length(), second.length());
+                }
+        );
+        System.out.println("sort on length using lambda3: " + Arrays.toString(strings));
+    }
+
+    public static <T> Collection<T> filter(Collection<T> items, Predicate<T> clause) {
+        Collection<T> result = new ArrayList<T>();
+        for (T item : items) {
+            if (clause.test(item)) {
+                result.add(item);
+            }
+        }
+        return result;
+    }
+
+    public static void useLambdaForPredicate() {
+        Collection<Integer> integers = Arrays.asList(0,1,2,3,4,5,6,7,8,9);
+        Collection<Integer> evens = filter(integers, x -> x % 2 == 0);
+        Collection<Integer> odds = filter(integers, x -> x % 2 == 1);
+        System.out.println("integers = " + Arrays.toString(integers.toArray()));
+        System.out.println("evens = " + Arrays.toString(evens.toArray()));
+        System.out.println("odds = " + Arrays.toString(odds.toArray()));
+    }
+
+
+    public static void main(String[] args) {
+        //useLambdaForComparator();
+        useLambdaForPredicate();
+    }
+}
---
+++ src/main/java/name/adhikari/sameer/playground/UserLombok.java
@@ -0,0 +1,40 @@
+package name.adhikari.sameer.playground;
+
+import lombok.*;
+
+/**
+ * Created by samadhik on 10/28/16.
+ * Modified by samadhik on 11/01/16.
+ */
+@Builder(builderClassName = "Builder", toBuilder = true)
+@Getter
+@Value
+public class UserLombok {
+    // Immutable class, all final attributes
+    private final String firstName; // Required
+    private final String lastName;  // Required
+    private final int age; // optional
+    private final String phone; // optional
+    private final String address; // optional
+
+    public static void main(String[] args) {
+        UserLombok user1 = UserLombok.builder()
+                .firstName("Spider")
+                .lastName("Man")
+                .age(42)
+                .phone("1234567890")
+                .address("1234 Awesome Rd")
+                .build();
+        System.out.println("user1 = " + user1);
+        UserLombok user2 = UserLombok.builder()
+                .firstName("Super")
+                .lastName("Woman")
+                .build();
+        System.out.println("user 2 = " + user2);
+        UserLombok user3 = user2.toBuilder()
+                .phone("9182736455")
+                .address("4567 Thrilling Ln.")
+                .build();
+        System.out.println("user3 = " + user3);
+    }
+}
