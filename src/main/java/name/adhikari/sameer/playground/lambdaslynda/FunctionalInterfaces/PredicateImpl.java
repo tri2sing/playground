ef27e2cff40276039f073fdf204b087653188a1b
Sameer Adhikari <sameer.adhikari@oracle.com>
2017-01-04 23:49:45 -0800

    Comparison of implementations

---
+++ src/main/java/name/adhikari/sameer/playground/lambdaslynda/FunctionalInterfaces/PredicateImpl.java
@@ -0,0 +1,29 @@
+package name.adhikari.sameer.playground.lambdaslynda.FunctionalInterfaces;
+
+import java.util.function.Predicate;
+
+public class PredicateImpl {
+
+    public class Classical implements Predicate<String>{
+        @Override
+        public boolean test(String input) {
+            return input.length() < 10? true: false;
+        }
+    }
+
+    public boolean lambda (String input) {
+        Predicate<String> lengthPredicate = (s) -> s.length() < 10;
+        return lengthPredicate.test(input);
+
+    }
+    public static void main(String[] args) {
+        PredicateImpl predicateLambda = new PredicateImpl();
+        PredicateImpl.Classical classical = predicateLambda.new Classical();
+
+        System.out.println("Lambda version: " + predicateLambda.lambda("Sameer") + " - Sameer is less than 10");
+        System.out.println("Classical version: " + classical.test("Sameer") + " - Sameer is less than 10");
+        System.out.println("Lambda version: " + predicateLambda.lambda("Anachronistically") + " - Anachronistically is less than 10");
+        System.out.println("Classical version: " + classical.test("Anachronistically") + " - Anachronistically is less than 10");
+
+    }
+}
