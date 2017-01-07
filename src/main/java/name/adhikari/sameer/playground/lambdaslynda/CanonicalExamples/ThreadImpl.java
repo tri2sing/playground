83c7d808022d320d738699b671ee0dd44ba15d42
Sameer Adhikari <sameer.adhikari@oracle.com>
2017-01-06 23:20:10 -0800

    completed thread example

--- src/main/java/name/adhikari/sameer/playground/lambdaslynda/CanonicalExamples/ThreadImpl.java
+++ src/main/java/name/adhikari/sameer/playground/lambdaslynda/CanonicalExamples/ThreadImpl.java
@@ -1,4 +1,19 @@
 package name.adhikari.sameer.playground.lambdaslynda.CanonicalExamples;
 
 public class ThreadImpl {
+    public static void main(String[] args) {
+        // Annonymous inner class
+        Runnable r1 = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Classical runnable");
+            }
+        };
+
+        Runnable r2 = () -> System.out.println("Lamda runnable");
+
+        r1.run();
+        r2.run();
+
+    }
 }
