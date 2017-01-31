c35d52f14f312e8a6cf5cde36079ffe02d0f1e51
Sameer Adhikari <sameer.adhikari@oracle.com>
2017-01-31 12:00:35 -0800

    outcomes for multiple bettors

--- src/main/java/montecarlo/Bettor.java
+++ src/main/java/montecarlo/Bettor.java
@@ -14,6 +14,9 @@ public class Bettor {
     public float placeWagers() {
         float resultingFunds = availableFunds;
         for (int i = 0; i < wagerAttempts; i++) {
+            if (resultingFunds < 0) {
+                return 0;
+            }
             if (Dice.rollDiceIsAWin()) {
                 resultingFunds += wagerAmount;
             } else {
@@ -24,8 +27,15 @@ public class Bettor {
     }
 
     public static void main(String[] args) {
-        Bettor bettor = new Bettor(100, 100, 10000);
-        float resultingFunds = bettor.placeWagers();
-        System.out.println(String.format("Initial amount = %.2f, final amount = %.2f", 10000.00, resultingFunds));
+        int numBettors = 100;
+        Bettor [] bettors = new Bettor[numBettors];
+        for (int i = 0; i < numBettors; i++) {
+            // As the wager attempts increase the likelihood of bettor going broke increases
+            bettors[i] = new Bettor(10000, 100, 10000);
+        }
+        for (Bettor bettor : bettors) {
+            float resultingFunds = bettor.placeWagers();
+            System.out.println(String.format("Initial = %8.2f, final = %8.2f", 10000.00, resultingFunds));
+        }
     }
 }
