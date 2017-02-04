7f45fa42789f23457e06c9e56a13488e03e0b941
Sameer Adhikari <sameer.adhikari@oracle.com>
2017-02-04 10:21:28 -0800

    replaced for loop with stream for adding data to series

--- src/main/java/montecarlo/Plotter.java
+++ src/main/java/montecarlo/Plotter.java
@@ -1,5 +1,6 @@
 package montecarlo;
 
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import javafx.application.Application;
@@ -23,26 +24,24 @@ public class Plotter extends Application {
         final LineChart<Number,Number> lineChart = new LineChart<>(xAxis,yAxis);
         lineChart.setTitle("Betting Outcome");
         lineChart.setCreateSymbols(false);
-        // TODO: figure out how make thin lines
-        //lineChart.setStyle("-fx-stroke-width: 100;");
 
-        int numBettors = 10;
+        // generating the data
+        int numBettors = 100;
         for (int i = 0; i < numBettors; i++) {
             XYChart.Series series = new XYChart.Series();
             series.setName(String.valueOf(i));
-            Bettor bettor = new Bettor(1000, 100, 1000);
+            lineChart.getData().add(series);
+
+            Bettor bettor = new Bettor(10000, 100, 10000);
             bettor.placeWagers();
             float [] progression = bettor.getWagerProgression();
-            for(int k = 0; k < progression.length; k++) {
-                series.getData().add(new XYChart.Data<>(k, progression[k]));
-            }
-            lineChart.getData().add(series);
-/*
-            IntStream.range(0, progression.length)
-                    .mapToObj(k -> series.getData().add(new XYChart.Data<>(k, progression[k])));
-*/
+            series.getData().addAll(
+                    IntStream.range(0, progression.length)
+                    .mapToObj(k -> new XYChart.Data<>(k, progression[k]))
+                    .collect(Collectors.toList())
+            );
         }
-        Scene scene  = new Scene(lineChart,800,600);
+        Scene scene  = new Scene(lineChart,1368, 768);
         stage.setScene(scene);
         stage.show();
     }
