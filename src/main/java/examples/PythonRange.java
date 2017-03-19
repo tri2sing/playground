7a062ed41ac5c5f3641d844048cf860ea4529466
Sameer Adhikari <sameer.adhikari@oracle.com>
2017-03-18 17:39:06 -0700

    quivalent of Python range

---
+++ src/main/java/examples/PythonRange.java
@@ -0,0 +1,14 @@
+package examples;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class PythonRange{
+    public static void main(String[] args){
+        List<Integer> list = IntStream.range(0, 10).boxed().collect(Collectors.toList());
+        System.out.println(list);
+        list = IntStream.rangeClosed(0, 10).boxed().collect(Collectors.toList());
+        System.out.println(list);
+    }
+}
