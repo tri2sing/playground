e1e04433920e0f5ff9025c7285f42e1f169ca137
Sameer Adhikari <sameer.adhikari@oracle.com>
2017-10-19 21:30:50 -0700

    thread interrupt

--- src/main/java/threads/Interrupter.java
+++ src/main/java/threads/Interrupter.java
@@ -9,14 +9,37 @@ public class Interrupter {
     private static class Worker implements Runnable {
         @Override
         public void run() {
-            try {
-                while (true) {
+            boolean done = false;
+            int iteration = 1;
+            while (!done) {
+                try {
                     Thread.sleep(3000);
+                    threadMessage("in iteration " + iteration++);
+                } catch (InterruptedException e) {
+                    threadMessage("I got interrupted");
+                    done = true;
                 }
-            } catch (InterruptedException e) {
-                threadMessage("I got interrupted");
             }
         }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        // maximum wait before interrupting the worker.
+        long maxWait = 1000 * 30; // half minute
+
+        threadMessage("Staring worker");
+        long startTime = System.currentTimeMillis();
+        Thread worker = new Thread(new Worker());
+        worker.start();
+        while(worker.isAlive()){
+            threadMessage("Waiting 5 secs for worker to exit");
+            worker.join(5000);
+            if (System.currentTimeMillis() - startTime > maxWait && worker.isAlive()) {
+                worker.interrupt();
+                worker.join();
+            }
+        }
+        threadMessage("Finally done");
     }
 
 }
